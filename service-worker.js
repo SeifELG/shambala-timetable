c78f11cca69c22fac5dcf1bbad293650c0e21fea
SeifELG <34184180+SeifELG@users.noreply.github.com>
2026-08-26 22:18:48 +0100

    time reactive timetable, global search, now and next

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "v2";
+const CACHE_VERSION = "v3";
 const CORE_CACHE = `shambala-core-${CACHE_VERSION}`;
 const MEDIA_CACHE = "shambala-media";
 const CACHE_PREFIX = "shambala-";
